dce8d7cb33e2f32012f6b44c5dd6b41e78cbe9c5
Saif Ali Shaik <saifshine7@gmail.com>
2021-03-02 17:48:11 +0530

    🐛 FIX: icon size

--- app/scripts/fullpage.js
+++ app/scripts/fullpage.js
@@ -12,7 +12,8 @@ async function renderApp() {
 function renderSidebar() {
   const dataMethBtn = document.querySelector('.btn-global-sidebar');
   dataMethBtn.addEventListener('fwClick', function getTktDetails() {
-    /** ~ playground start of ticket details page ~ */
+  /** ~ playground start of ticket details page ~ */
+    
     /** ~  end ~ */
   });
 }
